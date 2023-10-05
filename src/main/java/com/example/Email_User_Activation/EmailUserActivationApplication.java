19edc0e6d4f8bc392e4131f7553859989b04d769
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-10-05 09:10:59 +0430

    first commit

---
+++ src/main/java/com/example/Email_User_Activation/EmailUserActivationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Email_User_Activation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailUserActivationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailUserActivationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Email_User_Activation/EmailUserActivationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Email_User_Activation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailUserActivationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
